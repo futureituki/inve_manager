3fa4c24e60d29dfee16f9856d0aefcf161f8b9b1
CreateWebSiteTeam <yourgitemail@example.com>
2023-04-10 01:30:08 +0900

    first-commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  useEffect(() => {
+    const test = async() => {
+      await fetch('/').then((d) => {
+        console.log(`${d}成功`)
+      })
+    }
+    test()
+  },[])
+  return (
+    <div>Hello</div>
+  )
+}
