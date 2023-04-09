43b8fc02c61ff8e3b51c49eb9818eec30bb086a7
CreateWebSiteTeam <yourgitemail@example.com>
2023-04-10 01:58:42 +0900

    first-commit

---
+++ backends/src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { UserService } from './user.service';
+
+@UseGuards(AuthGuard('jwt'))
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService:UserService) {}
+  @Get()
+  getLoginUser(@Req() req:Request): Omit<User, 'hashedPassword'> {
+    return req.user
+  }
+}
