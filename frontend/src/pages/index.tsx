339794a4590bd6ef441470af19db7b3df3e2782f
CreateWebSiteTeam <yourgitemail@example.com>
2023-04-11 14:08:05 +0900

    feat:prettier設定

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useEffect } from 'react'
 
@@ -6,16 +5,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   useEffect(() => {
-    const test = async() => {
+    const test = async () => {
       await fetch('/').then((d) => {
         console.log(`${d}成功`)
       })
     }
     test()
-  },[])
-  return (
-    <div>
-      投資管理アプリです
-    </div>
-  )
+  }, [])
+  return <div>投資管理アプリです</div>
 }
