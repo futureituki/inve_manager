34c3d343d5d4852bc2b00d57e1fe289bb6e219f4
“Itsuki <satoitsuki0417@gmail.com>
2023-04-17 15:14:00 +0900

    fix: header_file Link > a delete

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -7,16 +7,12 @@ export const Header = () => {
     <header>
       <IconWrapper>
         <Link href="/">
-          <a>
             <HomeIcon className='h-8 w-8' />
-          </a>
         </Link>
         <Link href="/task">
-          <a>
             <p className='bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 w-fit'>
               取引を作成する
             </p>
-          </a>
         </Link>
       </IconWrapper>
     </header>
